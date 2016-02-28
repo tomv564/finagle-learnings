91e9867d8b1a924d5f939e8f26ed0fd68320c0e3
Tom van Ommeren <tom.vanommeren@gmail.com>
2016-02-28 20:36:24 +0100

    get a route json response to registrations

--- Server.scala
+++ Server.scala
@@ -3,8 +3,22 @@ import com.twitter.finagle.http.{Request, Response, Status}
 import com.twitter.util.{Await, Future}
 import com.twitter.finagle.http.service.RoutingService
 import com.twitter.finagle.http.path.{Root, /}
+import com.twitter.finagle.http.Method
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
+import java.io.ByteArrayOutputStream
+
+case class Registration (
+	chipNumber: String,
+	name: String,
+	category: String
+)
 
 object Main extends App {
+
+	val mapper = new ObjectMapper()
+	mapper.registerModule(DefaultScalaModule)
+
 
 	val alwaysOK = new Service[Request, Response] {
 	  def apply(req: Request): Future[Response] =
@@ -12,6 +26,9 @@ object Main extends App {
 	      Response(req.version, Status.Ok)
 	    )
 	}
+
+	val testPerson = Registration("AB1234", "Test Person", "M3040")
+	val registrations = List[Registration](testPerson)
 
 	def echoService(message: String) = new Service[Request, Response] {
 	    def apply(req: Request): Future[Response] = {
@@ -21,9 +38,21 @@ object Main extends App {
 	    }
 	  }
 
-	val router = RoutingService.byPathObject[Request] {
-	    // case Root / "user" / Integer(id) => userService(id)
-	    case Root / "echo"/ message => echoService(message)
+	def listRegistrationsService() = new Service[Request, Response] {
+		def apply(req: Request): Future[Response] = {
+
+			val out = new ByteArrayOutputStream
+			mapper.writeValue(out, registrations)
+		 	val r = Response(req.version, Status.Ok)
+		 	r.setContentTypeJson()
+		 	r.setContentString(out.toString())
+		 	Future(r)
+		}
+	}
+
+	val router = RoutingService.byMethodAndPathObject[Request] {
+	    case (Method.Post, Root / "echo" / message) => echoService(message)
+	    case (Method.Get, Root / "registrations") => listRegistrationsService
 	    case _ => alwaysOK
 	}
 
--- build.sbt
+++ build.sbt
@@ -2,3 +2,9 @@ name := "quickstart"
 version := "1.0"
 scalaVersion := "2.11.7"
 libraryDependencies += "com.twitter" %% "finagle-http" % "6.33.0"
+libraryDependencies ++= Seq(
+	"com.fasterxml.jackson.core" % "jackson-databind" % "2.6.3",
+	"com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.6.3"
+)
+
+
