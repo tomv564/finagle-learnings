292f5e1f5957369df499b62469f434a8a0d83d98
Tom van Ommeren <tom.vanommeren@gmail.com>
2016-04-07 09:38:48 +0200

    add listener impl for finagle-kestrel

--- results/build.sbt
+++ results/build.sbt
@@ -1,7 +1,7 @@
 resolvers += "Twitter's Repository" at "https://maven.twttr.com/"
 version := "1.0"
 scalaVersion := "2.11.7"
-// libraryDependencies += "com.twitter" %% "finagle-http" % "6.33.0"
+libraryDependencies += "com.twitter" %% "finagle-kestrel" % "6.34.0"
 libraryDependencies += "org.apache.thrift" % "libthrift" % "0.8.0"
 libraryDependencies += "com.twitter" %% "finagle-thrift" % "6.34.0"
 libraryDependencies += "com.twitter" %% "scrooge-core" % "4.6.0"
--- results/src/main/scala/TimingEventListener.scala
+++ results/src/main/scala/TimingEventListener.scala
@@ -25,6 +25,16 @@ import io.tomv.timing.registration.thrift.RegistrationService
 
 package io.tomv.timing.results {
 
+	import java.util.concurrent.atomic.AtomicBoolean
+
+	import com.twitter.finagle.builder.ClientBuilder
+	import com.twitter.finagle.kestrel.protocol.Kestrel
+	import com.twitter.finagle.kestrel.{ReadMessage, ReadHandle, Client}
+	import com.twitter.finagle.service.Backoff
+	import com.twitter.io.Buf
+	import com.twitter.util.JavaTimer
+	import net.lag.kestrel.QItem
+
 	class TimingEventListener(queue: PersistentQueue, results: mutable.ArrayBuffer[Result]) extends Runnable {
 
 		val events = mutable.MutableList[thrift.TimingEvent]()
@@ -33,14 +43,14 @@ package io.tomv.timing.results {
 
 		val timeout = 250.milliseconds.fromNow
 
+		//val stopped = new AtomicBoolean(false)
+
 		@volatile
 		var stopped : Boolean = false
 
 		def stop() : Unit = {
 			stopped = true
 		}
-
-//		listen(queue)
 
 		def createProtocol(bytes: Array[Byte]) : TProtocol = {
 			val buffer = new TMemoryBuffer(512)
@@ -50,25 +60,80 @@ package io.tomv.timing.results {
 
 			def run = {
 				stopped = false
-				listen()
+				listenLocal()
 			}
 
-	    def listen() : Unit = {
+	    def listenLocal() : Unit = {
 				while (!stopped) {
 					queue.waitRemove(Some(timeout), false).onSuccess { item =>
 						item.foreach { e =>
-							val prot = createProtocol(e.data)
-							val event = thrift.TimingEvent.decode(prot)
-							events += event
-							updateResults(events) foreach {
-								updatedResults =>
-									results.clear()
-									results ++= updatedResults
-							}
+							handleQItem(e)
 						}
 					}
 				}
 	    }
+
+			def handleMessage(msg: ReadMessage) : Unit = {
+				val bytes = new Array[Byte](msg.bytes.length)
+				msg.bytes.write(bytes, 0)
+				val buffer = new TMemoryBuffer(512)
+				buffer.write(bytes, 0, bytes.length)
+				val prot = new TBinaryProtocol(buffer)
+				val event = thrift.TimingEvent.decode(prot)
+				handleEvent(event)
+			}
+
+
+			def handleQItem(item: QItem) : Unit = {
+				val prot = createProtocol(item.data)
+				val event = thrift.TimingEvent.decode(prot)
+				handleEvent(event)
+			}
+
+		def handleEvent(event: thrift.TimingEvent): Unit = {
+			events += event
+			updateResults(events) foreach {
+				updatedResults =>
+					results.clear()
+					results ++= updatedResults
+			}
+
+		}
+
+
+
+			def listen() : Unit = {
+
+				// Add "host:port" pairs as needed
+				val host = "localhost:22133"
+
+				val client = Client(ClientBuilder()
+					.codec(Kestrel())
+					.hosts(host)
+					.hostConnectionLimit(1) // process at most 1 item per connection concurrently
+					.buildFactory())
+
+				val queueName = "queue"
+				val timer = new JavaTimer(isDaemon = true)
+				val retryBackoffs = Backoff.const(10.milliseconds)
+				val readHandle: ReadHandle = client.readReliably(queueName, timer, retryBackoffs)
+
+				// Attach an async error handler that prints to stderr
+				readHandle.error foreach { e =>
+					 System.err.println("zomg! got an error " + e)
+				}
+
+				// Attach an async message handler that prints the messages to stdout
+				readHandle.messages foreach { msg =>
+					try {
+						val Buf.Utf8(str) = msg.bytes
+						//handleItem(msg.bytes)
+					} finally {
+						msg.ack.sync() // if we don't do this, no more msgs will come to us
+					}
+				}
+
+			}
 
 	    def createResult(chipNumber: String, startEvent: thrift.TimingEvent, finishEvent: thrift.TimingEvent) : Future[Result] = {
 	      registrationClient.get(chipNumber) map {
